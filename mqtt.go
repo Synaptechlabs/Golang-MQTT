01fbfa1942a1e232489dd528e0ea9e5274b3a340
Scott Douglass <dev@synaptechlabs.ai>
2025-07-21 18:05:50 +1000

    some mods to 2025 standard

--- mqtt.go
+++ mqtt.go
@@ -1,43 +1,86 @@
-// Golang MQTT routing and forward IoT
 package main
 
 import (
-    "fmt"
-    MQTT "github.com/eclipse/paho.mqtt.golang"
-    "os"
-    "os/signal"
-    "syscall"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-var knt int
-var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
-    fmt.Printf("MSG: %s\n", msg.Payload())
-    text := fmt.Sprintf("this is result msg #%d!", knt)
-    knt++
-    token := client.Publish("nn/result", 0, false, text)
-    token.Wait()
+var counter int
+
+// MQTT message handler
+var messageHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("[RECV] %s | %s\n", msg.Topic(), msg.Payload())
+	response := fmt.Sprintf("Result message #%d", counter)
+	counter++
+
+	if client.IsConnected() {
+		pubToken := client.Publish("nn/result", 0, false, response)
+		pubToken.Wait()
+		if pubToken.Error() != nil {
+			fmt.Fprintf(os.Stderr, "[ERROR] Publish failed: %v\n", pubToken.Error())
+		} else {
+			fmt.Printf("[SEND] nn/result => %s\n", response)
+		}
+	} else {
+		fmt.Fprintln(os.Stderr, "[WARN] MQTT client disconnected. Cannot publish.")
+	}
 }
 
 func main() {
-    knt = 0
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	fmt.Println("[BOOT] Starting MQTT routing server...")
 
-    opts := MQTT.NewClientOptions().AddBroker("tcp://test.mosquitto.org:1883")
-    opts.SetClientID("wildcard")
-    opts.SetDefaultPublishHandler(f)
-    topic := "#"
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-    opts.OnConnect = func(c MQTT.Client) {
-            if token := c.Subscribe(topic, 0, f); token.Wait() && token.Error() != nil {
-                    panic(token.Error())
-            }
-    }
-    client := MQTT.NewClient(opts)
-    if token := client.Connect(); token.Wait() && token.Error() != nil {
-            panic(token.Error())
-    } else {
-            fmt.Printf("Connected to server\n")
-    }
-    <-c
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		fmt.Println("\n[INFO] Shutdown signal caught. Cleaning up...")
+		cancel()
+	}()
+
+	broker := "tcp://broker.hivemq.com:1883"
+	clientID := "synaptech-wildcard"
+	topic := "#"
+
+	opts := MQTT.NewClientOptions().
+		AddBroker(broker).
+		SetClientID(clientID).
+		SetKeepAlive(30 * time.Second).
+		SetPingTimeout(10 * time.Second).
+		SetAutoReconnect(true).
+		SetDefaultPublishHandler(messageHandler)
+
+	opts.OnConnect = func(c MQTT.Client) {
+	fmt.Printf("[INFO] Connected to broker: %s\n", broker)
+
+	token := c.Subscribe("#", 0, func(client MQTT.Client, msg MQTT.Message) {
+		fmt.Printf("[RECV] %s | %s\n", msg.Topic(), msg.Payload())
+	})
+	token.Wait()
+	if token.Error() != nil {
+		fmt.Fprintf(os.Stderr, "[ERROR] Subscribe failed: %v\n", token.Error())
+	} else {
+		fmt.Printf("[INFO] Subscribed to topic: %s\n", topic)
+	}
+}
+
+	client := MQTT.NewClient(opts)
+	connToken := client.Connect()
+	connToken.Wait()
+	if connToken.Error() != nil {
+		fmt.Fprintf(os.Stderr, "[FATAL] Initial connect failed: %v\n", connToken.Error())
+		os.Exit(1)
+	}
+
+	<-ctx.Done()
+	client.Disconnect(250)
+	fmt.Println("[INFO] Disconnected cleanly.")
 }
